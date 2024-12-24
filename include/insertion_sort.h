1dfdad82c60ec3c4f58934119999fd646a7d28ef
Davi Alves <davialvesr18@gmail.com>
2024-12-24 18:27:21 -0300

    update include

--- include/insertion_sort.h
+++ include/insertion_sort.h
@@ -1,3 +1,8 @@
+#ifndef INSERTION_SORT_H
+#define INSERTION_SORT_H
+
 #include <stdio.h>
 
 void insertion_sort(int v[], int tam);
+
+#endif
--- include/selection_sort.h
+++ include/selection_sort.h
@@ -1,3 +1,8 @@
+#ifndef SELECTION_SORT_H
+#define SELECTION_SORT_H
+
 #include <stdio.h>
 
 void selection_sort(int v[], int tam);
+
+#endif
